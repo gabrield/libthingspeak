e1b711594ba98fd0df810eedf7746aec6087e7e9
Gabriel Duarte <confusosk8@gmail.com>
2014-09-29 14:42:34 -0300

    replace \t by 4spaces

--- src/thingspeak.c
+++ src/thingspeak.c
@@ -4,127 +4,127 @@
 
 ts_context_t *ts_create_context(char *api_key, ts_feed_id_t feed_id)
 {
-	ts_context_t *ctx = NULL;
+    ts_context_t *ctx = NULL;
 
-	if(api_key != NULL)
-	{
-		ctx = (ts_context_t*)malloc(sizeof(ts_context_t));
-		bzero(ctx->api_key, 16);
-		strcpy(ctx->api_key, api_key);
-		ctx->api_key[16] = '\0';
-		ctx->feed_id = feed_id;
-	}
-	return ctx;
+    if(api_key != NULL)
+    {
+        ctx = (ts_context_t*)malloc(sizeof(ts_context_t));
+        bzero(ctx->api_key, 16);
+        strcpy(ctx->api_key, api_key);
+        ctx->api_key[16] = '\0';
+        ctx->feed_id = feed_id;
+    }
+    return ctx;
 }
 
 
 void ts_delete_context(ts_context_t *ctx)
 {
-	if(ctx)
-		free(ctx);
+    if(ctx)
+        free(ctx);
 }
 
 
 
 ts_datapoint_t *ts_set_value_i32(ts_datapoint_t *datapoint, int32_t val)
 {
-	datapoint->value_type = TS_VALUE_TYPE_I32;
-	datapoint->value.i32_value = val;
-	return datapoint;
+    datapoint->value_type = TS_VALUE_TYPE_I32;
+    datapoint->value.i32_value = val;
+    return datapoint;
 }
 
 
 int32_t ts_get_value_i32(ts_datapoint_t *datapoint)
 {
-	return datapoint->value.i32_value;
+    return datapoint->value.i32_value;
 }
 
 
 ts_datapoint_t *ts_set_value_f32(ts_datapoint_t *datapoint, float val)
 {
-	datapoint->value_type = TS_VALUE_TYPE_F32;
-	datapoint->value.f32_value = val;
-	return datapoint;
+    datapoint->value_type = TS_VALUE_TYPE_F32;
+    datapoint->value.f32_value = val;
+    return datapoint;
 }
 
 float ts_get_value_f32(ts_datapoint_t *datapoint)
 {
-	return datapoint->value.f32_value;
+    return datapoint->value.f32_value;
 }
 
 
 int32_t ts_datastream_update(ts_context_t* ctx, ts_feed_id_t feed_id, char * datastream_id, ts_datapoint_t *datapoint)
 {
-	char  num[128]    = {0};
-	ssize_t n         = 0;
-	ts_context_t* tsx = NULL;
+    char  num[128]    = {0};
+    ssize_t n         = 0;
+    ts_context_t* tsx = NULL;
 
 
-	switch(datapoint->value_type)
-	{
-		case TS_VALUE_TYPE_I32:
-			sprintf(num, "%s=%d", datastream_id, datapoint->value.i32_value);
-			break;
-		case TS_VALUE_TYPE_F32:
-			sprintf(num, "%s=%f", datastream_id, datapoint->value.f32_value);
-			break;
-		case TS_VALUE_TYPE_STR:
-			sprintf(num, "%s=%s", datastream_id, datapoint->value.str_value);
-			break;
-	}
+    switch(datapoint->value_type)
+    {
+        case TS_VALUE_TYPE_I32:
+            sprintf(num, "%s=%d", datastream_id, datapoint->value.i32_value);
+            break;
+        case TS_VALUE_TYPE_F32:
+            sprintf(num, "%s=%f", datastream_id, datapoint->value.f32_value);
+            break;
+        case TS_VALUE_TYPE_STR:
+            sprintf(num, "%s=%s", datastream_id, datapoint->value.str_value);
+            break;
+    }
 
 
-	if(feed_id == 0)
-	{
-		n = ts_http_post(ctx, HOST_API, "/update", num);
-	}
-	else
-	{
-		tsx = ts_create_context(ctx->api_key, feed_id);
-		if(tsx)
-		{
-			n = ts_http_post(ctx, HOST_API, "/update", num);
-			ts_delete_context(tsx);
-			return (n > 0) ? 0 : 1;
-		}
-		else
-			return -1;
-	}
-	return 0;
+    if(feed_id == 0)
+    {
+        n = ts_http_post(ctx, HOST_API, "/update", num);
+    }
+    else
+    {
+        tsx = ts_create_context(ctx->api_key, feed_id);
+        if(tsx)
+        {
+            n = ts_http_post(ctx, HOST_API, "/update", num);
+            ts_delete_context(tsx);
+            return (n > 0) ? 0 : 1;
+        }
+        else
+            return -1;
+    }
+    return 0;
 }
 
 
 
 char *ts_datastream_get(ts_context_t* ctx, ts_feed_id_t feed_id, ts_data_type_t type)
 {
-	char *ans          = NULL;
-	uint32_t id        = 0;
-	char  page[128] = {0};
-//	ts_context_t* tsx  = NULL;
+    char *ans          = NULL;
+    uint32_t id        = 0;
+    char  page[128] = {0};
+//    ts_context_t* tsx  = NULL;
 // http://api.thingspeak.com/channels/16258/feeds.json
 
 
-	id = (feed_id == 0) ? ctx->feed_id : feed_id;
+    id = (feed_id == 0) ? ctx->feed_id : feed_id;
 
-	switch(type)
-	{
-		case TS_DATA_CSV:
-			sprintf(page, "/channels/%d/feeds.csv", id);
-			break;
-		case TS_DATA_XML:
-			sprintf(page, "/channels/%d/feeds.xml", id);
-			break;
-		case TS_DATA_JSON:
-			sprintf(page, "/channels/%d/feeds.json", id);
-			break;
-	}
+    switch(type)
+    {
+        case TS_DATA_CSV:
+            sprintf(page, "/channels/%d/feeds.csv", id);
+            break;
+        case TS_DATA_XML:
+            sprintf(page, "/channels/%d/feeds.xml", id);
+            break;
+        case TS_DATA_JSON:
+            sprintf(page, "/channels/%d/feeds.json", id);
+            break;
+    }
 
 #if TS_DEBUG
-	printf("%s\n", page);
-#endif	
+    printf("%s\n", page);
+#endif    
 
-	ans = ts_http_get(HOST_API, page);
+    ans = ts_http_get(HOST_API, page);
 
 
-	return ans;
+    return ans;
 }
--- src/ts_http.c
+++ src/ts_http.c
@@ -4,133 +4,133 @@
 
 ssize_t ts_http_post(ts_context_t *ctx, char *host, char *page, char *poststr)
 {
-	int sockfd;
-	struct sockaddr_in servaddr;
-	char sendline[MAXLINE + 1], recvline[MAXLINE + 1];
-	ssize_t n;
-	struct hostent *hptr;
-	char hstr[50];
-	
+    int sockfd;
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE + 1], recvline[MAXLINE + 1];
+    ssize_t n;
+    struct hostent *hptr;
+    char hstr[50];
+    
 
 
-	if ((hptr = gethostbyname(host)) == NULL) {
-		fprintf(stderr, "gethostbyname error for host: %s: %s",
-			host, hstrerror(h_errno));
-		return -1;
-	}
+    if ((hptr = gethostbyname(host)) == NULL) {
+        fprintf(stderr, "gethostbyname error for host: %s: %s",
+            host, hstrerror(h_errno));
+        return -1;
+    }
 
-	if ((hptr->h_addrtype == AF_INET) && (hptr->h_addr_list) != NULL) {
-		inet_ntop(hptr->h_addrtype, hptr->h_addr_list[0], hstr, sizeof(hstr));
-	} 
-	else
-	{
-		fprintf(stderr, "Error call inet_ntop \n");
-	}
+    if ((hptr->h_addrtype == AF_INET) && (hptr->h_addr_list) != NULL) {
+        inet_ntop(hptr->h_addrtype, hptr->h_addr_list[0], hstr, sizeof(hstr));
+    } 
+    else
+    {
+        fprintf(stderr, "Error call inet_ntop \n");
+    }
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(80);
-	inet_pton(AF_INET, hstr, &servaddr.sin_addr);
-	connect(sockfd, (SA *) & servaddr, sizeof(servaddr));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(80);
+    inet_pton(AF_INET, hstr, &servaddr.sin_addr);
+    connect(sockfd, (SA *) & servaddr, sizeof(servaddr));
 
     
-	snprintf(sendline, MAXSUB,
-		 "POST %s HTTP/1.1\r\n"
-		 "Host: %s\r\n"
-		 "Connection: close\r\n"
-		 "X-THINGSPEAKAPIKEY: %s\r\n"
-		 "Content-Type: application/x-www-form-urlencoded\r\n"
-		 "Content-Length: %zu\r\n\r\n"
-		 "%s",
-		 page, 
-		 host,
-		 ctx->api_key,
-		 strlen(poststr),
-		 poststr);
+    snprintf(sendline, MAXSUB,
+         "POST %s HTTP/1.1\r\n"
+         "Host: %s\r\n"
+         "Connection: close\r\n"
+         "X-THINGSPEAKAPIKEY: %s\r\n"
+         "Content-Type: application/x-www-form-urlencoded\r\n"
+         "Content-Length: %zu\r\n\r\n"
+         "%s",
+         page, 
+         host,
+         ctx->api_key,
+         strlen(poststr),
+         poststr);
 
-#if TS_DEBUG	
-	printf("%s\n\n\n", sendline);
+#if TS_DEBUG    
+    printf("%s\n\n\n", sendline);
 #endif
 
-	write(sockfd, sendline, strlen(sendline));
-	
-	while ((n = read(sockfd, recvline, MAXLINE)) > 0)
-	{
-		recvline[n] = '\0';
-#if TS_DEBUG		
-		printf("%s", recvline);
+    write(sockfd, sendline, strlen(sendline));
+    
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0)
+    {
+        recvline[n] = '\0';
+#if TS_DEBUG        
+        printf("%s", recvline);
 #endif
-	}
-	close(sockfd);
+    }
+    close(sockfd);
 
-	return n;
+    return n;
 }
 
 
 
 char *ts_http_get(char *host, char *page)
 {
-	int sockfd;
-	FILE *sockwrap = NULL;
-	struct sockaddr_in servaddr;
-	char buffer[MAXLINE];
-	ssize_t n;
-	struct hostent *hptr;
-	char hstr[50];
-	char *http_ans = NULL;
-	int headers = 1;
-	
+    int sockfd;
+    FILE *sockwrap = NULL;
+    struct sockaddr_in servaddr;
+    char buffer[MAXLINE];
+    ssize_t n;
+    struct hostent *hptr;
+    char hstr[50];
+    char *http_ans = NULL;
+    int headers = 1;
+    
 
 
-	if ((hptr = gethostbyname(host)) == NULL) {
-		fprintf(stderr, "gethostbyname error for host: %s: %s",
-			host, hstrerror(h_errno));
-		return NULL;
-	}
+    if ((hptr = gethostbyname(host)) == NULL) {
+        fprintf(stderr, "gethostbyname error for host: %s: %s",
+            host, hstrerror(h_errno));
+        return NULL;
+    }
 
-#if TS_DEBUG	
-	printf("hostname: %s\n", hptr->h_name);
-#endif	
-	if ((hptr->h_addrtype == AF_INET) && (hptr->h_addr_list) != NULL) {
-		inet_ntop(hptr->h_addrtype, hptr->h_addr_list[0], hstr, sizeof(hstr));
-	} 
-	else
-	{
-		fprintf(stderr, "Error call inet_ntop \n");
-	}
+#if TS_DEBUG    
+    printf("hostname: %s\n", hptr->h_name);
+#endif    
+    if ((hptr->h_addrtype == AF_INET) && (hptr->h_addr_list) != NULL) {
+        inet_ntop(hptr->h_addrtype, hptr->h_addr_list[0], hstr, sizeof(hstr));
+    } 
+    else
+    {
+        fprintf(stderr, "Error call inet_ntop \n");
+    }
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(80);
-	inet_pton(AF_INET, hstr, &servaddr.sin_addr);
-	connect(sockfd, (SA *) & servaddr, sizeof(servaddr));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(80);
+    inet_pton(AF_INET, hstr, &servaddr.sin_addr);
+    connect(sockfd, (SA *) & servaddr, sizeof(servaddr));
 
     
-	sprintf(buffer, "GET %s HTTP/1.0\r\n"
+    sprintf(buffer, "GET %s HTTP/1.0\r\n"
                      "Host: %s\r\n\r\n", 
                      page, host);
 
-	n = strlen(buffer);
+    n = strlen(buffer);
     
     if(send(sockfd, buffer, strlen(buffer), 0) != n)
-    	fprintf(stderr, "Error call send()\n");
+            fprintf(stderr, "Error call send()\n");
 
     bzero(buffer, MAXLINE);
 
-     sockwrap = fdopen(sockfd, "r");
+    sockwrap = fdopen(sockfd, "r");
 
-
-     if (fgets(buffer, sizeof(buffer), sockwrap) == NULL)
-     {
-        if (ferror(sockwrap))
-           	fprintf(stderr, "Error sockwrap\n");
-        else
+    if(fgets(buffer, sizeof(buffer), sockwrap) == NULL)
+    {
+            if (ferror(sockwrap))
+                   fprintf(stderr, "Error sockwrap\n");
+            else
             fprintf(stderr, "No response: timeout\n");
     }
 
-     if (strstr(buffer, "200") == NULL) {
+    if(strstr(buffer, "200") == NULL)
+    {
         /* If fails, print the line */
         fprintf(stderr, "%s\n", buffer);
         return NULL;
@@ -142,10 +142,10 @@ char *ts_http_get(char *host, char *page)
     {
         if (fgets(buffer, sizeof(buffer), sockwrap) == NULL)
         {
-			if (ferror(sockwrap))
-	           	fprintf(stderr, "Error sockwrap\n");
-    	    else
-        	    fprintf(stderr, "No response: timeout\n");
+            if(ferror(sockwrap))
+                fprintf(stderr, "Error sockwrap\n");
+            else
+                fprintf(stderr, "No response: timeout\n");
         }
 
         if(strcmp("\r\n", buffer) == 0)
@@ -161,6 +161,6 @@ char *ts_http_get(char *host, char *page)
     close(sockfd);
 
     http_ans = (char*) buffer;
-	
-	return http_ans;
+    
+    return http_ans;
 }
